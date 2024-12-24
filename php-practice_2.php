1a86381545d9cd02956e81fecb7b6d3b075af91a
Hanae-96 <369liongreen@gmail.com>
2024-12-24 17:40:15 +0900

    オブジェクトの追加課題に解答しました。

--- php-practice_2.php
+++ php-practice_2.php
@@ -93,5 +93,50 @@ $current_date = new DateTime();
 
 $passed_days = $old_date->diff($current_date);
 echo $passed_days->format("あの日から%a日経過しました。")  . "\n";
+
+## Q1
+//商品クラスを使用して、条件を満たした上で下記の文字列を出力してください。
+### 条件
+// -コンストラクタメソッドを作成してください
+// -引数から `$param1` 受け取った商品名を `name` プロパティに格納してください
+// -引数から `$param2` 受け取った商品の価格を `price` プロパティに格納してください
+### 出力文字列
+//りんごの税抜価格は100円です。
+//クラス
+
+class Item
+{
+    public $name;
+    public $price;
     
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+    
+    public $includingTax;
+    
+    public function fetchCalTaxInPrice()
+    {
+        $this->includingTax = $this->price * 1.1;
+        echo $this->name . "の税込み価格は" . $this->includingTax ."円です。" . "\n";
+    }
+
+}
+
+$apple = new Item("りんご", 100);
+echo $apple->name . "の税抜価格は" . $apple->price . "円です。" . "\n";
+
+## Q2
+// Q1で使用した商品クラスを同じく使用して、条件を満たした上で下記の文字列を出力してください。
+### 条件
+// - priceを税込価格に変換して返すメソッド名は `fetchCalcTaxInPrice` と命名してください。
+// - fetchCalTaxInPriceメソッドの返り値を文字列結合して、最終的な文字列を完成させてください。
+### 出力文字列
+// みかんの税込価格は220円です。
+
+$orange = new Item("みかん", 200);
+$orange->fetchCalTaxInPrice();
+
 ?>
